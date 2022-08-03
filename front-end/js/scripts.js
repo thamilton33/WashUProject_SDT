b3ee9684627ed79db18c6b47864576698442b402
unknown <Danielfischer0708@gmail.com>
2022-08-03 08:47:45 -0500

    minor changes to front end

--- front-end/js/scripts.js
+++ front-end/js/scripts.js
@@ -7,6 +7,16 @@
 // Scripts
 // 
 
+let dateDropdown = document.getElementById('date-dropdown');
+let currentYear = new Date().getFullYear();
+let earliestYear = 1970;
+    while (currentYear >= earliestYear) {
+        let dateOption = document.createElement('option');
+        dateOption.text = currentYear;
+        dateOption.value = currentYear;
+        dateDropdown.add(dateOption);
+        currentYear -= 1;
+}
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
